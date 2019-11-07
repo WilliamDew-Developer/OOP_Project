c3b750325d1896b6727098e89c323ae5ab8b40fd
Nickalater <Nickalater@localhost>
2019-11-06 22:17:20 -0500

    First commit

---
+++ Weather1/src/Preflight.java
@@ -0,0 +1,15 @@
+
+public class Preflight {
+
+	public static void PreFlightStatus(String Condition) {
+		
+		if (Condition == "Clear") {
+			//TakeoffProtocol
+			System.out.println("Conditions are clear for takeoff");		
+		} else {
+			System.out.println("Ground planes. Conditions are not favorable for flight");
+			DelayFlights.GroundFlights();
+			//If conditions aren't favorable. Send to method that delays all flights 
+		}
+	}
+}
